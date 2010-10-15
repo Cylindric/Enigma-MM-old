540ea91ce177e1601e279a3a3a9948e57f19027c
Cylindric <mark@hanfordonline.co.uk>
2010-10-15 21:29:42 +0100

    Removed obj files

---
+++ source/Solution/Server/ComandParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnigmaMM
+{
+    class ComandParser
+    {
+        public String ParseCommand(String Command)
+        {
+            String ReturnValue = "";
+
+            switch (Command)
+            {
+                case "stop":
+                    ReturnValue = "Server stopped";
+                    break;
+
+                case "list":
+                    ReturnValue = "Online Users: Dave, Dee, Dozy";
+                    break;
+            }
+
+            return ReturnValue;
+        }
+    }
+}
