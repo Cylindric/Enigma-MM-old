b328cf2f65e6d9c6a62cfaa1d68abd9d96c07893
Cylindric <mark@hanfordonline.co.uk>
2010-10-22 13:18:03 +0100

    Created script to build a quick debug environment

---
+++ source/Solution/BuildDebugEnvironment.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+SETLOCAL
+:START
+
+SET SCRIPTPATH=%~dp0
+SET SCRIPTDRIVE=%SCRIPTPATH:~0,2%
+
+
+::Set some common paths
+SET REPOSITORY=D:\Minecraft\MCServerRoot
+SET ROOT=%SCRIPTPATH%
+SET MCSROOT=%ROOT%\Build
+SET MCROOT=%MCSROOT%\Minecraft
+SET SMROOT=%MCSROOT%\ServerManager
+SET AVROOT=%MCSROOT%\AlphaVespucci
+SET MAPROOT=%MCSROOT%\Maps
+
+:: Change to the script's path
+%SCRIPTDRIVE%
+CD %SCRIPTPATH%
+
+:: Create the folder hieararchy
+IF NOT EXIST "%MCSROOT%" MKDIR "%MCSROOT%"
+IF NOT EXIST "%MCROOT%" MKDIR "%MCROOT%"
+IF NOT EXIST "%SMROOT%" MKDIR "%SMROOT%"
+IF NOT EXIST "%AVROOT%" MKDIR "%AVROOT%"
+IF NOT EXIST "%MAPROOT%" MKDIR "%MAPROOT%"
+
+::Copy the files we need to the package
+XCOPY /Y "%REPOSITORY%\Minecraft\minecraft_server.jar" "%MCROOT%"
+XCOPY /Y "%REPOSITORY%\AlphaVespucci\*" "%AVROOT%"
+
+:END
+PAUSE
+ENDLOCAL
