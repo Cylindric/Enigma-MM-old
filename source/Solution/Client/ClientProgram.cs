b3cbf927f7e74145f359cd34b780c6d2a0da3861
Cylindric <mark@hanfordonline.co.uk>
2010-10-22 17:33:15 +0100

    Added old Client Server code back in

---
+++ source/Solution/Client/ClientProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnigmaMM
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+             Client C = new Client();
+            C.StartClient();
+
+            // Loop and send test commands
+            int i;
+            for (i = 0; i < 10; i++)
+            {
+                C.SendData("This is a really long test message sure to exceed the length of the receiving buffer " + i);
+                C.SendData("and a little bit extra " + i);
+                //C.SendData("xxxxxx");
+                System.Threading.Thread.Sleep(1000);
+            }
+            C.StopClient();
+       }
+    }
+}
---
+++ source/Solution/EMM/CSocketPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace EnigmaMM
+ {
+     public class CSocketPacket
+     {
+         public const int BufferSize = 32;
+         public System.Net.Sockets.Socket thisSocket;
+         public byte[] dataBuffer = new byte[BufferSize];
+     }
+ }
