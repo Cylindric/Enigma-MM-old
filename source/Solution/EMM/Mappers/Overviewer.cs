eab838aa43b0c33e3fc14bae413cc63b5c32d622
Cylindric <mark@hanfordonline.co.uk>
2010-11-03 08:05:51 +0000

    Couple of fixes to the mappers

--- source/Solution/EMM/Mappers/Overviewer.cs
+++ source/Solution/EMM/Mappers/Overviewer.cs
@@ -1,5 +1,7 @@
 ﻿using System.IO;
 using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
 
 namespace EnigmaMM
 {
@@ -28,19 +30,61 @@ namespace EnigmaMM
             }
 
             string cmd = string.Format(
-                "\"{0}\" -p 1 --cachedir \"{1}\" \"{2}\" \"{3}\"",
+                "\"{0}\" --cachedir \"{1}\" \"{2}\" \"{3}\"",
                 mExePath, mCachePath, mMinecraft.ServerProperties.WorldPath, mOutputPath
             );
 
             Process p = new Process();
             p.StartInfo.FileName = Config.PythonExe;
             p.StartInfo.UseShellExecute = false;
-            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.CreateNoWindow = false;
             p.StartInfo.Arguments = cmd;
             p.Start();
             p.WaitForExit();
 
+            CreateMarkersFromWarpLocations();
+
             mMinecraft.RaiseServerMessage("OVERVIEWER: Done.");
         }
+
+
+        private void CreateMarkersFromWarpLocations()
+        {
+            StringBuilder markers = new StringBuilder();
+            markers.Append("var markerData=[\n");
+            foreach (KeyValuePair<string, string> kvp in mMinecraft.ServerWarps.Settings)
+            {
+                string[] warp = kvp.Value.Split(':');
+
+                string name = kvp.Key;
+                float x = 0f;
+                float y = 0f;
+                float z = 0f;
+                string a = "";
+                string b = "";
+                string group = "";
+
+                float.TryParse(warp[0], out x);
+                float.TryParse(warp[1], out y);
+                float.TryParse(warp[2], out z);
+                a = warp[3];
+                b = warp[4];
+                group = warp[5];
+
+                if ((group == "") || (group == "users"))
+                {
+                    markers.Append("    {");
+                    markers.AppendFormat("\"msg\": \"{0}\", ", name);
+                    markers.AppendFormat("\"y\": {0:f0}, \"z\": {1:f0}, \"x\": {2:f0}", y, z, x);
+                    markers.Append("}, \n");
+                }
+            }
+            markers.Append("];\n");
+
+            StreamWriter sw = File.CreateText(Path.Combine(mOutputPath, "markers.js"));
+            sw.Write(markers.ToString());
+            sw.Close();
+        }
+
     }
 }
