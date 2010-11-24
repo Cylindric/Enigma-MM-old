ce555a6a53c6ae0a5202977f81635366abd68c55
Cylindric <mark@hanfordonline.co.uk>
2010-11-24 13:55:42 +0000

    Push to version 1.0.0.0

--- source/Solution/Server/Properties/AssemblyInfo.cs
+++ source/Solution/Server/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Enigma Minecraft Manager")]
-[assembly: AssemblyCopyright("Copyright ©  2010 Mark Hanford")]
+[assembly: AssemblyCopyright("Copyright Mark Hanford© 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
