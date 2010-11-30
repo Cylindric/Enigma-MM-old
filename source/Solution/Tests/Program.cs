a85be6c6f8ae4a4ab77670efa3a5f0ce65536420
Cylindric <mark@hanfordonline.co.uk>
2010-11-30 18:28:39 +0000

    Added Unit Testing framework

---
+++ source/Solution/Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace EnigmaMM.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scheduler.ScheduleTaskTests t = new Scheduler.ScheduleTaskTests();
+            t.TestAllDays();
+            t.TestAlternateDays();
+            t.TestEveryHour();
+            t.TestNextDay();
+            t.TestNextRun();
+        }
+    }
+}
