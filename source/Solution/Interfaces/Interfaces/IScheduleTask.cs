c8a4ade01e8dc85504932715fe80e4369e0de11e
Cylindric <mark@hanfordonline.co.uk>
2010-12-24 09:04:02 +0000

    Big refactor to remove statics, and to prepare for Plugins

---
+++ source/Solution/Interfaces/Interfaces/IScheduleTask.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace EnigmaMM.Interfaces
+{
+    /// <summary>
+    /// Provides an interface for Scheduled Tasks.
+    /// </summary>
+    public interface IScheduleTask
+    {
+        /// <summary>
+        /// Gets or sets the name for this task.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the command to run when the task executes.
+        /// </summary>
+        string Command { get; set; }
+
+        /// <summary>
+        /// Gets the time this task is next set to run.
+        /// </summary>
+        DateTime NextRun { get; }
+
+        /// <summary>
+        /// Gets the time this task is next set to run, formatted in natural language.
+        /// </summary>
+        string NextRunString { get; }
+
+        /// <summary>
+        /// Gets or sets the Days part of the task's schedule.
+        /// </summary>
+        string RunDays { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Hours part of the task's schedule.
+        /// </summary>
+        string RunHours { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Minutes part of the task's schedule.
+        /// </summary>
+        string RunMinutes { get; set; }
+
+        /// <summary>
+        /// Calculates the next time this task is due to run, after the current time.
+        /// </summary>
+        /// <returns>The calculated next run time.</returns>
+        DateTime CalculateNextRunTime();
+
+        /// <summary>
+        /// Calculates the next time this task is due to run, after the specified time.
+        /// </summary>
+        /// <param name="start">The time to start calculating from.</param>
+        /// <returns>The calculated next run time.</returns>
+        DateTime CalculateNextRunTime(DateTime start);
+    }
+}
---
+++ source/Solution/Interfaces/Interfaces/IServerSettings.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnigmaMM.Interfaces
+{
+    public interface IServerSettings
+    {
+        /// <summary>
+        /// Initialises the Settings class and loads the settings from the file specified.
+        /// </summary>
+        /// <param name="fileName">The full path to the settings file.</param>
+        void Initialise(string fileName);
+
+        bool Loaded { get; }
+
+        /// <summary>
+        /// Returns the filename of the currently-used settings file.
+        /// </summary>
+        string Filename { get; }
+
+        /// <summary>
+        /// Returns the full path to the Server Manager.
+        /// </summary>
+        string ServerManagerRoot { get; }
+
+        /// <summary>
+        /// Returns the full path to the directory to use for cache files.
+        /// </summary>
+        string CacheRoot { get; }
+
+        /// <summary>
+        /// Returns the full path to the folder to use for backups.
+        /// </summary>
+        string BackupRoot { get; }
+
+        /// <summary>
+        /// Returns the full path to the folder where Minecraft is installed.
+        /// </summary>
+        string MinecraftRoot { get; }
+
+        /// <summary>
+        /// Returns the executable to execute the Java files.
+        /// </summary>
+        string JavaExec { get; }
+
+        /// <summary>
+        /// Returns the jar to use to launch Minecraft.
+        /// </summary>
+        string ServerJar { get; }
+
+        /// <summary>
+        /// Returns the initial amount of memory, in megabytes, to allocate to the Java heap.
+        /// </summary>
+        int JavaHeapInit { get; }
+
+        /// <summary>
+        /// Returns the maximum amount of memory, in megabytes, to allow the Java heap.
+        /// </summary>
+        int JavaHeapMax { get; }
+
+        string MapRoot { get; }
+
+        int OptimisePng { get; }
+
+        string GetString(string key, string defaultValue);
+        string GetString(string key);
+        int GetInt(string key, int defaultValue);
+        int GetInt(string key);
+        bool GetBool(string key, bool defaultValue);
+        bool GetBool(string key);
+        string GetRootedPath(string root, string key, string defaultValue);
+        string GetRootedPath(string root, string key);
+
+    }
+}
