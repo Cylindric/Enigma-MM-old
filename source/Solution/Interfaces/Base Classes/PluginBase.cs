a5f386d711299bac982235acd60076f3caea97c9
Cylindric <mark@hanfordonline.co.uk>
2010-12-24 09:04:02 +0000

    Big refactor to remove statics, and to prepare for Plugins

---
+++ source/Solution/Interfaces/Base Classes/PluginBase.cs
@@ -0,0 +1,7 @@
+﻿namespace EnigmaMM.Interfaces
+{
+    public abstract class PluginBase
+    {
+        public abstract string Name { get; }
+    }
+}
---
+++ source/Solution/Interfaces/Interfaces/IMCSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EnigmaMM.Interfaces
+{
+    public interface IMCSettings
+    {
+        Dictionary<string, string> Values { get; }
+        string Filename { get; }
+        bool AutoLoad { get; }
+        bool Monsters { get; }
+        bool OnlineMode { get; }
+        int MaxPlayers { get; }
+        int ServerPort { get; }
+        string LevelName { get; }
+        string ServerIp { get; }
+        string WorldPath { get; }
+
+        void LookForNewSettings();
+        bool Load();
+    }
+}
