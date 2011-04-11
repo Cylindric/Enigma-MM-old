fa8a979b561ed61ef9bf8afffb26c018f5253e08
Cylindric <mark@hanfordonline.co.uk>
2011-04-11 17:42:29 +0100

    Various plugin changes

--- source/Solution/Plugin.c10t/Plugin.c10t.cs
+++ source/Solution/Plugin.c10t/Plugin.c10t.cs
@@ -19,6 +19,26 @@ namespace EnigmaMM.Plugin.Implementation
 
         public override void Render()
         {
+            Render("main");
+        }
+
+        public override void Render(params string[] args)
+        {
+            // If no args specified, just run the default task
+            if (args.Length == 0)
+            {
+                Render("main");
+            }
+
+            foreach (string arg in args)
+            {
+                RenderMap(arg);
+            }
+        }
+
+
+        private void RenderMap(string type) 
+        {
             // Get and check that the Executable exists
             string exeFile = PluginSettings.GetRootedPath(Server.Settings.ServerManagerRoot, "ExePath", @".\c10t\c10t.exe");
             if (!File.Exists(exeFile))
@@ -33,18 +53,25 @@ namespace EnigmaMM.Plugin.Implementation
             // Check the output path
             VerifyPath(Path.GetDirectoryName(OutputPath), false);
             VerifyPath(OutputPath, true);
-            OutputPath = Path.Combine(OutputPath, "default.png");
 
-            // Check the cache path
+            string OutputFile = Path.Combine(OutputPath, type+".png");
+
+            // Check the core cache path
             VerifyPath(Path.GetDirectoryName(CachePath), false);
             VerifyPath(CachePath, true);
 
             Server.RaiseServerMessage("{0}: Rendering map...", this.Name);
 
+            string renderParameters = PluginSettings.GetString(type + ".RenderParams", "--isometric --show-players");
             string cmd = string.Format(
-                "--world \"{0}\" --output \"{1}\"",
-                WorldPath, OutputPath
+                "--world \"$WORLD\" --output \"$OUTPUTFILE\" --cache-dir \"$CACHE\" --cache-key {0} {1}",
+                type, renderParameters
             );
+
+            cmd = cmd.Replace("$WORLD", WorldPath);
+            cmd = cmd.Replace("$CACHE", CachePath);
+            cmd = cmd.Replace("$OUTPUTPATH", OutputPath);
+            cmd = cmd.Replace("$OUTPUTFILE", OutputFile);
 
             Process p = new Process();
             p.StartInfo.FileName = exeFile;
@@ -54,6 +81,10 @@ namespace EnigmaMM.Plugin.Implementation
             p.Start();
             p.PriorityClass = ProcessPriorityClass.BelowNormal;
             p.WaitForExit();
+
+            int smallWidth = PluginSettings.GetInt(type + ".smallWidth", 250);
+            string smallFile = Path.Combine(OutputPath, type + "-small.png");
+            this.Resize(OutputFile, smallFile, smallWidth);
 
             Server.RaiseServerMessage("{0}: Done.", this.Name);
         }
