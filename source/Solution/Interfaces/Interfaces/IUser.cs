ac1c6fbfac55ce077838ec5717ea9586a6d04aac
Cylindric <mark@hanfordonline.co.uk>
2011-04-17 23:06:31 +0100

    More database conversion

--- source/Solution/Interfaces/Interfaces/IUser.cs
+++ source/Solution/Interfaces/Interfaces/IUser.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EnigmaMM
+namespace EnigmaMM.Interfaces
 {
-    class User: Data.User, Interfaces.IUser
+    public interface IUser
     {
     }
 }
